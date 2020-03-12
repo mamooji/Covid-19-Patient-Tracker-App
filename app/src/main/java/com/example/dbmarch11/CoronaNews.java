418c7947e5f7b94f7bdc510c106e5b628e78e4ce
Mohamed Benzreba <Mbenzreba7150@conestogac.on.ca>
2020-03-12 19:49:31 -0400

    Setting up CoronaNews activity basic widget status and adding strings.xml values to it as well

--- app/src/main/java/com/example/dbmarch11/CoronaNews.java
+++ app/src/main/java/com/example/dbmarch11/CoronaNews.java
@@ -1,14 +1,36 @@
 package com.example.dbmarch11;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 
 public class CoronaNews extends AppCompatActivity {
 
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView rvRSS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_corona_news);
+
+        // Set widget & layout access
+        this.swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        this.rvRSS = (RecyclerView) findViewById(R.id.rv_rss);
+
+        // Set a linear layout for our recycler view
+        this.rvRSS.setLayoutManager(new LinearLayoutManager(this));
+
+        // Setting the refresh listener
+        this.swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Call the AsyncTask to fetch the RSS feed for us
+                // new FetchFeedTask().execute((Void) null);
+            }
+        });
     }
 }
